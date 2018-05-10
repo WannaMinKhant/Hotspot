3e8ec0828401b30189627d06bd6a3437853981c9
WannaMinKhant <wannakyawhtin17@gmail.com>
2018-05-10 11:42:03 +0630

    hotspot app

---
+++ Virtual_WIFI_Csharp/Virtual_WIFI_Csharp/frmHotspot.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Virtual_WIFI_Csharp
+{
+    partial class frmHotspot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHotspot));
+            this.txtSSID = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Play_Stop_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSSID
+            // 
+            this.txtSSID.Location = new System.Drawing.Point(35, 59);
+            this.txtSSID.Name = "txtSSID";
+            this.txtSSID.Size = new System.Drawing.Size(247, 20);
+            this.txtSSID.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(35, 121);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '☻';
+            this.txtPass.Size = new System.Drawing.Size(247, 20);
+            this.txtPass.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(35, 163);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "View Password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SSID Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // Play_Stop_Button
+            // 
+            this.Play_Stop_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Play_Stop_Button.Location = new System.Drawing.Point(110, 208);
+            this.Play_Stop_Button.Name = "Play_Stop_Button";
+            this.Play_Stop_Button.Size = new System.Drawing.Size(75, 23);
+            this.Play_Stop_Button.TabIndex = 5;
+            this.Play_Stop_Button.Text = "Start";
+            this.Play_Stop_Button.UseVisualStyleBackColor = true;
+            this.Play_Stop_Button.Click += new System.EventHandler(this.Play_Stop_Button_Click);
+            // 
+            // Hotspot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 247);
+            this.Controls.Add(this.Play_Stop_Button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtSSID);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Hotspot";
+            this.Text = "WIFI_ Hotspot by FS";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSSID;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Play_Stop_Button;
+    }
+}
+
---
+++ Virtual_WIFI_Csharp/Virtual_WIFI_Csharp/frmHotspot.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.IO;
+
+
+namespace Virtual_WIFI_Csharp
+{
+    public partial class frmHotspot : Form
+    {
+        Process newProcess = new Process();
+        bool connect = false;
+        string oldSSID_oldPass;
+        string oldSSID, oldPass;
+        string[] readLine;
+        public frmHotspot()
+        {
+           
+            InitializeComponent();
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                txtPass.PasswordChar = '\0';
+            }
+            else 
+            {
+                txtPass.PasswordChar = '☻';
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader sReader = new StreamReader(Application.StartupPath + "\\config.txt");
+                oldSSID_oldPass = sReader.ReadLine();
+                readLine = oldSSID_oldPass.Split(',');
+                oldSSID = readLine[0];
+                oldPass = readLine[1];
+                sReader.Close();
+                txtSSID.Text = oldSSID;
+                txtPass.Text = oldPass;
+                if (!IsUseAdmin())
+                {
+                    Process_Start_1();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n Don't worry this file was created by Progam when you was starting hotspot","Warnning",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+        }
+
+
+        public static bool IsUseAdmin()
+        {
+            WindowsIdentity user = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principle = new WindowsPrincipal(user);
+            return principle.IsInRole(WindowsBuiltInRole.Administrator);
+
+        }
+
+        public void Process_Start_1()
+        {
+            ProcessStartInfo startinfo = new ProcessStartInfo();
+            startinfo.UseShellExecute = true;
+            startinfo.CreateNoWindow = true;
+            startinfo.WorkingDirectory = Environment .CurrentDirectory;
+            startinfo.FileName = System.Windows.Forms.Application.ExecutablePath;
+            startinfo.Verb = "runas";
+
+            try 
+            {
+                Process process = Process.Start(startinfo);
+            
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            System.Windows.Forms.Application.Exit();
+        }
+
+        
+         private void Play_Stop_Button_Click(object sender, EventArgs e)
+         {
+             string ssid = txtSSID.Text, pass = txtPass.Text;
+
+             if (!connect)
+             {
+                 if (String.IsNullOrEmpty(ssid))
+                 {
+                     MessageBox.Show("Enter SSID ");
+
+                 }
+                 else
+                 {
+                     if (txtPass.Text == null || txtPass.Text == "")
+                     {
+                         MessageBox.Show("Enter Password");
+                     }
+                     else
+                     {
+                         if (txtPass.TextLength >= 8)
+                         {
+                             Hotspot(ssid, pass, true);
+                             txtSSID.Enabled = false;
+                             txtPass.Enabled = false;
+                             Play_Stop_Button.Text = "Stop";
+                             connect = true;
+                             try
+                             {
+                                 //FileStream fstream = new FileStream(Application.StartupPath + "\\config.ini", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                                 StreamWriter sWriter = new StreamWriter(Application.StartupPath + "\\config.txt");
+                                 sWriter.WriteLine(ssid + "," + pass);
+                                 sWriter.Close();
+                             }
+                             catch(Exception ex)
+                             {
+                                 MessageBox.Show(ex.Message);
+                             }
+                         }
+                         else
+                         {
+                             MessageBox.Show("Password must be larger than 8 character");
+                         }
+                     }
+
+                 }
+
+             }
+             else 
+             {
+                 Hotspot(null, null, false);
+                 txtSSID.Enabled = true;
+                 txtPass.Enabled = true;
+                 Play_Stop_Button.Text = "Start";
+             }
+
+         }
+
+         private void Hotspot(string ssid, string key, bool status)
+         {
+             ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe");
+             processStartInfo.RedirectStandardInput = true;
+             processStartInfo.RedirectStandardOutput = true;
+             processStartInfo.CreateNoWindow = true;
+             processStartInfo.UseShellExecute = false;
+             Process process = Process.Start(processStartInfo);
+
+             if (process != null)
+             {
+                 if (status)
+                 {
+                     process.StandardInput.WriteLine("netsh wlan set hostednetwork mode=allow ssid=" + ssid + " key=" + key);
+                     process.StandardInput.WriteLine("netsh wlan start hosted network");
+                     process.StandardInput.Close();
+                 }
+                 else
+                 {
+                     process.StandardInput.WriteLine("netsh wlan stop hostednetwork");
+                     process.StandardInput.Close();
+                 }
+             }
+         }
+
+         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+         {
+             
+         }
+
+         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+         {
+             Hotspot(null, null, false);
+             Application.Exit();
+         }
+
+    }
+
+}
